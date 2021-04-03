154259fc23002341f1540785c985d328673b637a
Ilya Shevchyk <il2001sh@gmail.com>
2021-04-03 14:48:27 +0300

    entry point

--- js/main.js
+++ js/main.js
@@ -6,14 +6,20 @@ function run() {
     document.getElementById('general').innerHTML = 
     `<kbd>${(q(input)/31).toFixed(4)}</kbd>`;
 
-    document.getElementById('group').innerHTML = 
+    document.getElementById('fact1').innerHTML = 
     `<kbd>${(q1(input)/3).toFixed(4)}</kbd>`;
 
-    document.getElementById('rest').innerHTML = 
-    `<kbd>${(q2(input)/24).toFixed(4)}</kbd>`;
+    document.getElementById('fact2').innerHTML = 
+    `<kbd>${(q2(input)/7).toFixed(4)}</kbd>`;
 
-    document.getElementById('fisher').innerHTML = 
-    `<kbd>${((q1(input)/3)/(q2(input)/24)).toFixed(4)}</kbd>`;
+    document.getElementById('rest').innerHTML = 
+    `<kbd>${(q3(input)/21).toFixed(4)}</kbd>`;
+
+    document.getElementById('fisher1').innerHTML = 
+    `<kbd>${((q1(input)/3)/(q3(input)/21)).toFixed(4)}</kbd>`;
+    
+    document.getElementById('fisher2').innerHTML = 
+    `<kbd>${((q2(input)/7)/(q3(input)/21)).toFixed(4)}</kbd>`;
 
     document.getElementById('table').innerHTML = 
     `<kbd>${(tableValue).toFixed(4)}</kbd>`;
