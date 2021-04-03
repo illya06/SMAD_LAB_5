9eccaf6abf606b4192d0a39ef1bbd2d37e2806dd
Ilya Shevchyk <il2001sh@gmail.com>
2021-04-03 14:37:18 +0300

    dispersion evaluation

--- js/logic.js
+++ js/logic.js
@@ -35,39 +35,50 @@ function x(data) {
     return res / 32
 }
 
-// //general dispersion
-// function q(data) {
-//     let x = xGeneral(data)
-//     let res = 0
+//general dispersion
+function q(data) {
+    let xGeneral = x(data)
+    let res = 0
 
-//     for (let i = 0; i < 4; i++)
-//         for (let j = 0; j < 8; j++)
-//             res += Math.pow(data[i][j] - x, 2)
+    for (let i = 0; i < 4; i++)
+        for (let j = 0; j < 8; j++)
+            res += Math.pow(data[i][j] - xGeneral, 2)
 
-//     return res
-// }
+    return res
+}
 
-// //group dispersion
-// function q1(data) {
-//     let xG = xGeneral(data)
-//     let res = 0
+//group 1 dispersion
+function q1(data) {
+    let xG = x(data)
+    let res = 0
 
-//     for (let i = 0; i < 4; i++)
-//         res += Math.pow(x(data[i]) - xG, 2)
+    for (let i = 0; i < 4; i++)
+        res += Math.pow(x1(data, i) - xG, 2)
 
-//     return res * 8
-// }
+    return res * 8
+}
 
-// //in group dispersion
-// function q2(data) {
-//     let res = 0
+//group 2 dispersion
+function q2(data) {
+    let xG = x(data)
+    let res = 0
 
-//     for (let i = 0; i < 4; i++)
-//         for (let j = 0; j < 8; j++)
-//             res += Math.pow(data[i][j] - x(data[i]), 2)
+    for (let i = 0; i < 8; i++)
+        res += Math.pow(x2(data, i) - xG, 2)
 
-//     return res
-// }
+    return res * 4
+}
+
+//zalyshkova
+function q3(data) {
+    let res = 0
+
+    for (let i = 0; i < 4; i++)
+        for (let j = 0; j < 8; j++)
+            res += Math.pow(data[i][j] - x1(data, i) - x2(data, j) + x(data), 2)
+
+    return res
+}
 
 // function comp() {
 //     let ret = ''
